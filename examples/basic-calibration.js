923d0866325ff96dddcb2a873e30f85e702284d2
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-27 09:57:47 +0100

    Fix code style and linting

--- examples/basic-calibration.js
+++ examples/basic-calibration.js
@@ -1,65 +1,67 @@
+'use strict';
+
 const Hapi = require('hapi');
 const Calibrate = require('calibrate');
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
 
 server.register([
-  { register: Calibrate.decorate }    // Register decorator, used in third example
+    { register: Calibrate.decorate }    // Register decorator, used in third example
 ], (err) => {
 
-  if (err) {
-    throw err;
-  }
-
-  server.route([
-    {
-      method: 'GET',
-      path: '/user/{id}',
-      handler: function (request, reply) {            // Using Promises
-
-        const promise = User.findById(request.params.id)
-          .then(Calibrate.response)               // Formats Response
-          .catch(Calibrate.error);                // Errors caught and wrapped
-        return reply(promise);                      // Return Calibrated Response
-      }
-    },
-    {
-      method: 'GET',
-      path: '/team/{id}',
-      handler: function (request, reply) {            // Using Callbacks
-
-        Team.findById(request.params.id, (err, team) => {
-
-          if (err) {                              // Catch any errors
-            return reply(Calibrate.error(err)); // Errors caught and wrapped
-          }
-
-          return reply(Calibrate.response(team)); // Return Calibrate Response
-        });
-      }
-    },
-    {
-      method: 'GET',
-      path: '/team/{id}',
-      handler: function (request, reply) {            // Using new decorator function
-
-        Team.findById(request.params.id, (err, team) => {
-
-          if(err) {                               // Catch any errors
-            return reply.calibrate(err);        // Using decorator function
-          }
-
-          return reply.calibrate(team);           // Using decorator function
-        });
-      }
-    }
-  ]);
-
-  server.start((err) => {
-
     if (err) {
-      throw err;
+        throw err;
     }
-    console.log('Server running at:', server.info.uri);
-  });
+
+    server.route([
+        {
+            method: 'GET',
+            path: '/user/{id}',
+            handler: function (request, reply) {            // Using Promises
+
+                const promise = User.findById(request.params.id)
+                    .then(Calibrate.response)               // Formats Response
+                    .catch(Calibrate.error);                // Errors caught and wrapped
+                return reply(promise);                      // Return Calibrated Response
+            }
+        },
+        {
+            method: 'GET',
+            path: '/team/{id}',
+            handler: function (request, reply) {            // Using Callbacks
+
+                Team.findById(request.params.id, (err, team) => {
+
+                    if (err) {                              // Catch any errors
+                        return reply(Calibrate.error(err)); // Errors caught and wrapped
+                    }
+
+                    return reply(Calibrate.response(team)); // Return Calibrate Response
+                });
+            }
+        },
+        {
+            method: 'GET',
+            path: '/team/{id}',
+            handler: function (request, reply) {            // Using new decorator function
+
+                Team.findById(request.params.id, (err, team) => {
+
+                    if (err) {                               // Catch any errors
+                        return reply.calibrate(err);        // Using decorator function
+                    }
+
+                    return reply.calibrate(team);           // Using decorator function
+                });
+            }
+        }
+    ]);
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        console.log('Server running at:', server.info.uri);
+    });
 });
